ea8ef9371f9300eabfbbc22bf59cc3be6980c253
girlshawn35 <GirlShawn35@MacBook-Pro.local>
2016-05-27 11:52:20 -0700

    adds script

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/*
+Project Name: Relaxr Blog
+Client: Homework Assignment #4
+Author: Shawn Aperocho
+Developer @GA in LA
+*/
+console.log("loaded");
+
+    $(document).ready(function() {
+    	event.preventDefault()
+    });
